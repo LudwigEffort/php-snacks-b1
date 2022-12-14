77b0a8e4a766e3b6e6bb679c0896ab514da119a2
LudwigEffort <dr.feynman65@gmail.com>
2022-12-15 00:34:44 +0100

    snack 2 done

--- snack_2/index.php
+++ snack_2/index.php
@@ -1,3 +1,28 @@
+<?php
+    if (isset($_GET['firstName']) && 
+        isset($_GET['email']) &&
+        isset($_GET['age']) ) {
+        $name = $_GET['firstName'];
+        $mail = $_GET['email'];
+        $age = $_GET['age'];
+    } 
+    else {
+        $name = '';
+        $mail = '';
+        $age = '';
+    };
+
+    $pos_at = strpos($mail, '@');
+    $pos_dot = strpos($mail, '.', $pos_at);
+
+    if ($pos_at && $pos_dot && (strlen($name)>3) && is_numeric($age) ) {
+        $message = 'login successful';
+    }
+    else {
+        $message = 'login unsuccessful';
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +33,15 @@
 </head>
 <body>
     <h1>Snack 2</h1>
+    <form action="" method="get">
+        <label for="userFirstName">First name:</label>
+        <input type="text" name="firstName" id="userFirstName">
+        <label for="userEmail">Email:</label>
+        <input type="text" name="email" id="userEmail">
+        <label for="userAge">Age:</label>
+        <input type="text" name="age" id="userAge">
+        <button>Submit</button>
+    </form>
+    <h1><?php echo $message ?></h1>
 </body>
 </html>
