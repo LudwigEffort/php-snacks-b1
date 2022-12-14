ceedf6d8d3ce4645121f9ad299ce98279876c4d1
LudwigEffort <dr.feynman65@gmail.com>
2022-12-14 22:56:15 +0100

    snack 1 done

--- snack_1/index.php
+++ snack_1/index.php
@@ -8,5 +8,87 @@
 </head>
 <body>
     <h1>Snack 1</h1>
+    <h2>NBA Matchs Sab 10/12/2022</h2>
+
+    <?php
+    $saturday = [
+
+        //Match 1
+        [
+            "team1" => "Heat",
+            "team2" => "Spurs",
+            "score" => "111|115",
+        ],
+
+        //Match 2
+        [
+            "team1" => "Mavericks",
+            "team2" => "Bucks",
+            "score" => "105|106",
+        ],
+
+        //Match 3
+        [
+            "team1" => "Jazz",
+            "team2" => "Timberwolves",
+            "score" => "108|118",
+        ],
+
+        //Match 4
+        [
+            "team1" => "Pelicans",
+            "team2" => "Suns",
+            "score" => "128|117",
+        ],
+
+        //Match 5
+        [
+            "team1" => "Grizzilies",
+            "team2" => "Pistons",
+            "score" => "114|103",
+        ],
+
+        //Match 6
+        [
+            "team1" => "Cavaliers",
+            "team2" => "Kings",
+            "score" =>"95|105",
+        ]
+
+    ];
+    ?>
+    <h4>Match list:</h4>
+    <ul>
+        <li>
+            <span><?php print_r($saturday[0]["team1"]); ?></span> vs 
+            <span><?php print_r($saturday[0]["team2"]); ?></span>,
+            Score: <span><?php print_r($saturday[0]["score"]); ?></span>
+        </li>
+        <li>
+            <span><?php print_r($saturday[1]["team1"]); ?></span> vs 
+            <span><?php print_r($saturday[1]["team2"]); ?></span>,
+            Score: <span><?php print_r($saturday[1]["score"]); ?></span>
+        </li>
+        <li>
+            <span><?php print_r($saturday[2]["team1"]); ?></span> vs 
+            <span><?php print_r($saturday[2]["team2"]); ?></span>,
+            Score: <span><?php print_r($saturday[2]["score"]); ?></span>
+        </li>
+        <li>
+            <span><?php print_r($saturday[3]["team1"]); ?></span> vs 
+            <span><?php print_r($saturday[3]["team2"]); ?></span>,
+            Score: <span><?php print_r($saturday[3]["score"]); ?></span>
+        </li>
+        <li>
+            <span><?php print_r($saturday[4]["team1"]); ?></span> vs 
+            <span><?php print_r($saturday[4]["team2"]); ?></span>,
+            Score: <span><?php print_r($saturday[4]["score"]); ?></span>
+        </li>
+        <li>
+            <span><?php print_r($saturday[5]["team1"]); ?></span> vs 
+            <span><?php print_r($saturday[5]["team2"]); ?></span>,
+            Score: <span><?php print_r($saturday[5]["score"]); ?></span>
+        </li>
+    </ul>
 </body>
 </html>
